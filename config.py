cab4c54d676e623f169697dec04ed2a7453f4936
CanzyKing <rioevalion2@gmail.com>
2025-02-04 17:58:32 +0800

    config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+# Token Telegram Bot
+TOKEN = '7580009941:AAGJrFrmUVfSah_LC7dqlATrOKgmA5KyFYQ'  # Ganti dengan token bot Anda
+IMAGE_URL = 'https://i.imgur.com/uzDwClk.jpg'  # URL gambar yang akan dikirim
+
+# Global variables
+blocked_proxies = set()  # Daftar proxy yang sudah terblokir
+attack_status = {}
